08364c1939cebaa989e610f031ba2b77afd17a6c
CristianM <crism.klee@gmail.com>
2024-06-22 13:37:18 -0300

    Devuelve codigo 201 personalizado p/post

--- src/main/java/tech/challenge/foroHub/controller/TopicoController.java
+++ src/main/java/tech/challenge/foroHub/controller/TopicoController.java
@@ -4,11 +4,15 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import tech.challenge.foroHub.domain.Curso.CursoRepository;
 import tech.challenge.foroHub.domain.Topico.DatosRegistroTopico;
+import tech.challenge.foroHub.domain.Topico.DatosRespuestaTopico;
 import tech.challenge.foroHub.domain.Topico.Topico;
 import tech.challenge.foroHub.domain.Topico.TopicoRepository;
 import tech.challenge.foroHub.domain.Usuario.UsuarioRepository;
+
+import java.net.URI;
 
 
 @RestController
@@ -25,12 +29,20 @@ public class TopicoController {
     private UsuarioRepository usuarioRepository;
 
     @PostMapping
-    public void registrarTopico(@RequestBody @Valid DatosRegistroTopico datosRegistroTopico){
+    public ResponseEntity<DatosRespuestaTopico> registrarTopico(@RequestBody @Valid DatosRegistroTopico datosRegistroTopico, UriComponentsBuilder uriComponentsBuilder){
 
         var topico = new Topico(datosRegistroTopico);
         topico.setCurso(cursoRepository.findByNombreContainsIgnoreCase(datosRegistroTopico.nombreCurso()).get());
         topico.setAutor(usuarioRepository.findById(datosRegistroTopico.usuario_id()).get());
-        topicoRepository.save(topico);
+        Topico topicoRet = topicoRepository.save(topico);
+
+        DatosRespuestaTopico datosRespuestaTopico = new DatosRespuestaTopico(topicoRet.getId(), topicoRet.getTitulo(),
+                topicoRet.getMensaje(), topicoRet.getFecha_creacion().toString(), topicoRet.getEstado().toString(),
+                topicoRet.getCurso().getId(), topicoRet.getAutor().getId());
+
+        URI url = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topicoRet.getId()).toUri();
+
+        return ResponseEntity.created(url).body(datosRespuestaTopico);
 
     }
 
