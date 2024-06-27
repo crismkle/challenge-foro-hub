4e4d3fb4c7b6316621ca4fb1295d24210b8b519b
CristianM <crism.klee@gmail.com>
2024-06-27 14:11:32 -0300

    Cambios en tablas

--- src/main/resources/db/migration/V8__insert-into-table-usuarios.sql
+++ src/main/resources/db/migration/V8__insert-into-table-usuarios.sql
@@ -1,6 +1,12 @@
 INSERT INTO usuarios(nombre, email, clave, perfil)
-VALUES('Nicolás Pacheco', 'nico54@gmail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'ESTUDIANTE');
+VALUES('Nicolás Pacheco', 'nico54@mail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'ESTUDIANTE');
 INSERT INTO usuarios(nombre, email, clave, perfil)
-VALUES('Paula Marcos', 'paumarcos@gmail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'AYUDANTE');
+VALUES('Paula Marcos', 'paumarcos@mail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'AYUDANTE');
 INSERT INTO usuarios(nombre, email, clave, perfil)
-VALUES('Martín Lopez', 'lopemartin@gmail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'ESTUDIANTE');
+VALUES('Martín Lopez', 'lopemartin@mail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'ESTUDIANTE');
+INSERT INTO usuarios(nombre, email, clave, perfil)
+VALUES('Florencia Carmona', 'carmoflopy@mail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'ESTUDIANTE');
+INSERT INTO usuarios(nombre, email, clave, perfil)
+VALUES('Lautaro Flores', 'lauflo666@mail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'ESTUDIANTE');
+INSERT INTO usuarios(nombre, email, clave, perfil)
+VALUES('Rodrigo Nuñez', 'rodrinunez@mail.com', '$2a$12$fOJIAu8BE07kY2FE8Pr1ze5s.R6Lh71NVeXsxKy3nAqWXw1PHLeEO', 'ESTUDIANTE');
